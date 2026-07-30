b74d683e8ce80c0b9c0999c54b7a7ca98d52b3c0
Blessing-hue736/GET-324-mini-project-group-fe3 <blessingukpe85@gmail.com>
2026-07-30 19:48:49 +0100

    Update project group name in sidebar

--- App.py
+++ App.py
@@ -17,7 +17,7 @@ with st.sidebar:
     st.header("ABOUT THIS PROJECT")
     st.write(
         "**GET 324 Mini-Project**\n\n"
-        "Group FE3\n\n"
+        "Group FE 2\n\n"
         "FOOD ENGINEERING\n\n"
         "TASK: Healthy Tomato Leaf vs Tomato Early Blight"
     )
